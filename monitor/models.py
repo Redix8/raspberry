08d424a5fd3c927f129115c1f6e80f53733866bb
redix8 <kukko@paran.com>
2020-06-30 11:04:55 +0900

    django model init

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class PlantOneEnviron(models.Model):
+    pass
+
+class PlantTwoEnviron(models.Model):
+    pass
+
+class WeatherForecast(models.Model):
+    pass
+
+class Prediction(models.Model):
+    pass
+
