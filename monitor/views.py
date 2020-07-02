9d633b10c167e6195a969511f49465ec5042fef1
unknown <kka960602@gmail.com>
2020-07-02 22:56:42 +0900

    plotly_testing2

--- monitor/views.py
+++ monitor/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from plotly.offline import plot #plotly
 from plotly.graph_objs import *
-import plotly.plotly as py
+import plotly.graph_objs as go
 
 from .models import PlantEnviron, WeatherForecast
 # Create your views here.
@@ -17,22 +17,30 @@ def index(request):
 
 def line(request):
     # articles = Article.objects.all()
-    envs = WeatherForecast.objects.all() 
+    envs = PlantEnviron.objects.all() 
     # sample -> object 1개 -> sample.plant -> 1 / 2
     #env_df = read_frame(envs, fieldnames=['recTime', 'tem_in_loc1','hum_in_loc1','tem_coil_loc1'])
-    env_df = envs.to_dataframe(['fcTime','temp_25','humid_25','rain_25'])
-    # for sample in samples:
-    #     x = sample.lc[2:5]
-    #     y = sample[6:]
+    env_df = envs.to_dataframe(['recTime','temp_25','humid_25','rain_25'])
+    # env_df['fcTime'] = env_df['fcTime'].to_datetime()
     x_data = env_df.iloc[:24,:1]
     y_data = env_df.iloc[:24,1:]
+    print(x_data)
+    print(y_data)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['tem_in_loc1'], mode='lines+markers', name='온도',
+                 opacity=0.8, marker_color='red'))
 
-    layout = Layout(plot_bgcolor='rgba(0,0,0,0)')
-    data = Data([Scatter(x=x_data.index, y=y_data['temp_25'], mode='lines', name='test',
-    opacity=0.8, marker_color='green')], output_type='div')
-    # context = {
-    #     'plot_div': plot_div,
-    # }
-    fig = Figure(data=data, layout=layout)
-    plot(fig, output_type='div')
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['hum_in_loc1'], mode='lines+markers', name='습도',
+                 opacity=0.8, marker_color='green'))
+
+    fig.add_trace(go.Bar(x=x_data.index, y=y_data['tem_coil_loc1'], name='강수확률',
+                 opacity=0.8))
+    fig.update_layout(title='<b>공장 환경 관측</b>', xaxis_title='Date', yaxis_title='Scale')          
+
+    plot_div = plot(fig, output_type='div')
+    context = {
+            'plot_div': plot_div,
+            'x' : x_data,
+            'y' : y_data,
+        }
     return render(request, 'monitor/line.html', context)
