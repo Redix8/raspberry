b832e1b15da8c34ea245d16448c5d439c0730201
Redix8 <40425965+Redix8@users.noreply.github.com>
2020-07-03 22:00:09 +0900

    visualization

--- monitor/views.py
+++ monitor/views.py
@@ -26,9 +26,43 @@ def plant(request, plant):
 
 @login_required
 def loc(request, plant, loc):
+
+    # 현재값 + 예측값
+    # plant_envs = Prediction.objects.all().order_by('recTime').last()
+    plant_all = Prediction.objects.all().filter(plant=plant)
+    cnt = len(plant_all) - 1
+    plant_envs = plant_all[cnt]
+
+    # 공장 환경 : 현재 값-24시간 + 현재 기준으로 24, 48예측 값
+    envs = PlantEnviron.objects.all().filter(plant=plant)
+    # sample -> object 1개 -> sample.plant -> 1 / 2
+    # env_df = read_frame(envs, fieldnames=['recTime', 'tem_in_loc1','hum_in_loc1','tem_coil_loc1'])
+    env_df = envs.to_dataframe(['recTime', 'tem_in_loc1', 'hum_in_loc1', 'tem_coil_loc1'])
+    # env_df['fcTime'] = env_df['fcTime'].to_datetime()
+    x_data = env_df.iloc[:24, :1]
+    y_data = env_df.iloc[:24, 1:]
+    # print(x_data)
+    # print(y_data)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['tem_in_loc1'], mode='lines+markers', name='온도',
+                             opacity=0.8, marker_color='red'))
+
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['hum_in_loc1'], mode='lines+markers', name='습도',
+                             opacity=0.8, marker_color='green'))
+
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['tem_coil_loc1'], mode='lines+markers', name='코일 온도',
+                             opacity=0.8))
+    fig.update_layout(title='<b>Today Factory Environment</b>', xaxis_title='Date', yaxis_title='Scale')
+
+    plot_div = plot(fig, output_type='div')
+
     context = {
         'plant': plant,
         'loc': loc,
+        'plant_envs': plant_envs,
+        'plot_div': plot_div,
+        'x': x_data,
+        'y': y_data,
     }
     return render(request, 'monitor/loc.html', context)
 
