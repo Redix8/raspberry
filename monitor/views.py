1f8627a43e3e85c4d620ea78e7e1713ffa19e3b9
unknown <kka960602@gmail.com>
2020-07-03 09:29:22 +0900

    monitor/line

--- monitor/views.py
+++ monitor/views.py
@@ -17,23 +17,23 @@ def index(request):
 
 def line(request):
     # articles = Article.objects.all()
-    envs = PlantEnviron.objects.all() 
+    envs = WeatherForecast.objects.all() 
     # sample -> object 1개 -> sample.plant -> 1 / 2
     #env_df = read_frame(envs, fieldnames=['recTime', 'tem_in_loc1','hum_in_loc1','tem_coil_loc1'])
-    env_df = envs.to_dataframe(['recTime','temp_25','humid_25','rain_25'])
+    env_df = envs.to_dataframe(['fcTime','temp_25','humid_25','rain_25'])
     # env_df['fcTime'] = env_df['fcTime'].to_datetime()
     x_data = env_df.iloc[:24,:1]
     y_data = env_df.iloc[:24,1:]
     print(x_data)
     print(y_data)
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['tem_in_loc1'], mode='lines+markers', name='온도',
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['temp_25'], mode='lines+markers', name='온도',
                  opacity=0.8, marker_color='red'))
 
-    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['hum_in_loc1'], mode='lines+markers', name='습도',
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['humid_25'], mode='lines+markers', name='습도',
                  opacity=0.8, marker_color='green'))
 
-    fig.add_trace(go.Bar(x=x_data.index, y=y_data['tem_coil_loc1'], name='강수확률',
+    fig.add_trace(go.Bar(x=x_data.index, y=y_data['rain_25'], name='강수확률',
                  opacity=0.8))
     fig.update_layout(title='<b>공장 환경 관측</b>', xaxis_title='Date', yaxis_title='Scale')          
 
