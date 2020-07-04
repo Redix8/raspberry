fee3691a5645b495358db51269881b9c6ecdb10b
chojunhyung <reqip95@gmail.com>
2020-07-04 17:13:28 +0900

    Update UI

--- monitor/views.py
+++ monitor/views.py
@@ -11,10 +11,9 @@ from .models import PlantEnviron, WeatherForecast, Prediction
 @login_required
 def index(request):
     context = {
-        'plant': 0
+        'plant': 1
     }
-
-    return render(request, 'monitor/index.html', context)
+    return render(request, 'monitor/plant.html', context)
 
 @login_required
 def plant(request, plant):
@@ -37,20 +36,20 @@ def loc(request, plant, loc):
     envs = PlantEnviron.objects.all().filter(plant=plant)
     # sample -> object 1개 -> sample.plant -> 1 / 2
     # env_df = read_frame(envs, fieldnames=['recTime', 'tem_in_loc1','hum_in_loc1','tem_coil_loc1'])
-    env_df = envs.to_dataframe(['recTime', 'tem_in_loc1', 'hum_in_loc1', 'tem_coil_loc1'])
+    env_df = envs.to_dataframe(['recTime', f'tem_in_loc{loc}', f'hum_in_loc{loc}', f'tem_coil_loc{loc}'])
     # env_df['fcTime'] = env_df['fcTime'].to_datetime()
     x_data = env_df.iloc[:24, :1]
     y_data = env_df.iloc[:24, 1:]
     # print(x_data)
     # print(y_data)
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['tem_in_loc1'], mode='lines+markers', name='온도',
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data[f'tem_in_loc{loc}'], mode='lines+markers', name='온도',
                              opacity=0.8, marker_color='red'))
 
-    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['hum_in_loc1'], mode='lines+markers', name='습도',
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data[f'hum_in_loc{loc}'], mode='lines+markers', name='습도',
                              opacity=0.8, marker_color='green'))
 
-    fig.add_trace(go.Scatter(x=x_data.index, y=y_data['tem_coil_loc1'], mode='lines+markers', name='코일 온도',
+    fig.add_trace(go.Scatter(x=x_data.index, y=y_data[f'tem_coil_loc{loc}'], mode='lines+markers', name='코일 온도',
                              opacity=0.8))
     fig.update_layout(title='<b>Today Factory Environment</b>', xaxis_title='Date', yaxis_title='Scale')
 
